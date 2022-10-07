58d156fc06e72936eb18c7596459823d2481c0c6
JockeMedKniven <JockeMedKniven@users.noreply.github.com>
2022-10-07 15:53:11 +0200

    day1

---
+++ Labb 2 - Kitchen Appliance/Köksapparater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Labb2;
+
+namespace Labb_2___Kitchen_Appliance
+{
+    public class Köksapparater : IKitchenAppliance
+    {
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public bool IsFunctioning { get; set; }
+        public void Use()
+        {
+
+        }
+    }
+    public class K : Köksapparater
+    {
+
+    }
+
+}
--- Labb 2 - Kitchen Appliance/Program.cs
+++ Labb 2 - Kitchen Appliance/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Labb_2___Kitchen_Appliance;
+var kitchenAppliance = new Köksapparater();
+List<string[]> apparater = new List<string[]>();
+
+
+bool startMenu = true;
+while (startMenu)
+{
+    //Skriver ut meny med vår menymetod
+    PrintMenu();
+    Int32.TryParse(Console.ReadLine(), out int selectedOption);
+    switch (selectedOption)
+    {
+        //Använd köksapparat
+        case 1:
+
+            break;
+
+        //Lägg till en köksapparat
+        case 2:
+            break;
+
+        //Lista köksapparater
+        case 3:
+            break;
+
+        //Ta bort kökapparat
+        case 4:
+            break;
+
+        //Stäng av programmet
+        case 5:
+            startMenu = false;
+            break;
+
+            //Visar något vid felinmatning
+        default:
+            Console.WriteLine("Error, lär dig läsa menyn!");
+            break;
+    }
+}
+
+void PrintMenu()
+{
+    Console.Beep();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("\t=======Menu=======" +
+    "\n\t1. Use Appliance" +
+    "\n\t2. Add Kitchen Appliance" +
+    "\n\t3. List Kitchen Appliances" +
+    "\n\t4. Remove Appliance" +
+    "\n\t5. Exit" +
+    "\n\t>");
+}
+void ApplianceUsage(List<string> list)
+{
+    Console.WriteLine("Which appliance would you like to use?");
+    
+}
+void AddUserInputAppliance()
+{
+
+}
+string[] AddAppliance(string Type, string Brand)
+{
+
+}
